bce4156e1b7a58718ea7fd175e0bfbd68a55a3e7
Oleg Dudnik <oleggelo86@gmail.com>
2025-06-19 13:10:44 +0300

    ✅ (tests): Add unit-tests for Pipe, Splitter and Pipeline classes

---
+++ tests/test_pipes.py
@@ -0,0 +1,52 @@
+from pipes_and_filters.pipes.pipe import Pipe
+from pipes_and_filters.pipes.splitter import Splitter
+from pipes_and_filters.pipeline import Pipeline
+
+
+def test_pipe():
+
+    f = Pipe(
+        lambda x: x + 3,
+        lambda x: x * 2,
+        lambda x: x - 4,
+        lambda x: x / 2
+    )
+
+    assert f(5) == 6
+
+
+def test_splitter():
+
+    f = Pipe(
+        lambda x: 3 * x + 1,
+        lambda x: x // 2,
+    )
+
+    f1 = Pipe(
+        lambda x: x // 2,
+        lambda x: x // 2
+    )
+
+    f2 = Pipe(
+        lambda x: 3 * x + 1,
+        lambda x: 3 * x + 1
+    )
+
+    actual = Splitter(
+        input_pipe=f,
+        outputs_pipes=[f1, f2]
+    )
+
+    assert actual(5) == [2, 76]
+
+
+def test_pipeline():
+
+    pipeline = Pipeline(
+        source=range(10),
+        pipe=lambda x: x ** 2
+    )
+
+    expected = [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+
+    assert list(pipeline()) == expected

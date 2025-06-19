593dddfa1dd128b8f82dd17c5bfe1dea5a3d7d9b
Oleg Dudnik <oleggelo86@gmail.com>
2025-06-19 13:08:24 +0300

    📦️ (package): Update __init__ routers

---
+++ pipes_and_filters/__init__.py
@@ -0,0 +1,10 @@
+from pipes_and_filters.pipes import Pipe
+from pipes_and_filters.pipes import Splitter
+from pipes_and_filters.pipeline import Pipeline
+
+
+__all__ = [
+    'Pipe',
+    'Splitter',
+    'Pipeline'
+]
---
+++ pipes_and_filters/pipes/__init__.py
@@ -0,0 +1,8 @@
+from pipes_and_filters.pipes.pipe import Pipe
+from pipes_and_filters.pipes.splitter import Splitter
+
+
+__all__ = [
+    'Pipe',
+    'Splitter'
+]

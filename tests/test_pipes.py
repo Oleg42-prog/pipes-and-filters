3c81c6a15fecf2aa5d64f4e98154dbbdc2318604
Oleg Dudnik <Oleggelo86@gmail.com>
2025-06-23 00:47:03 +0300

    ✅ (tests): Add unit-test for Flow class

--- tests/test_pipes.py
+++ tests/test_pipes.py
@@ -1,6 +1,8 @@
 from pipes_and_filters.pipes.pipe import Pipe
 from pipes_and_filters.pipes.splitter import Splitter
 from pipes_and_filters.pipeline import Pipeline
+from pipes_and_filters.flow import Flow
+from pipes_and_filters.utils import identity
 
 
 def test_pipe():
@@ -50,3 +52,20 @@ def test_pipeline():
     expected = [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
 
     assert list(pipeline()) == expected
+
+
+def test_flow():
+
+    flow = Flow(
+        source=range(1, 6),
+        splitter=Splitter(
+            input_pipe=identity,
+            outputs_pipes=[
+                lambda x: x,
+                lambda x: x ** 2
+            ]
+        ),
+        sink=lambda x, y: x / y
+    )
+
+    assert list(flow()) == [1, 2/4, 3/9, 4/16, 5/25]
